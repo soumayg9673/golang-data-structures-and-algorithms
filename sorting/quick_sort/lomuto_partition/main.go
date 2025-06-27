e641e745e4d015845fcdee3d424430536ef290c5
soumayg9673 <pmrs9673@gmail.com>
2025-06-27 22:38:47 +0530

    [sorting] quick sort using lomuto partition

--- sorting/quick_sort/lomuto_partition/main.go
+++ sorting/quick_sort/lomuto_partition/main.go
@@ -2,11 +2,8 @@ package main
 
 import "fmt"
 
-/*
-	Considering pivot the last element of the array
-*/
-
-func lomutoParition(arr []int, l, h int) {
+// Considering pivot the last element of the array
+func lomutoParition(arr []int, l, h int) int {
 	p := arr[h]
 	i := l - 1
 	for j := l; j <= h-1; j++ {
@@ -16,11 +13,20 @@ func lomutoParition(arr []int, l, h int) {
 		}
 	}
 	arr[i+1], arr[h] = arr[h], arr[i+1]
-	fmt.Println(arr)
+	return i + 1
+}
+
+func quickSort(arr []int, l, h int) {
+	if l < h {
+		p := lomutoParition(arr, l, h)
+		quickSort(arr, l, p-1)
+		quickSort(arr, p+1, h)
+	}
 }
 
 func main() {
 	arr1 := []int{10, 80, 30, 90, 40, 50, 70}
 
-	lomutoParition(arr1, 0, len(arr1)-1)
+	quickSort(arr1, 0, len(arr1)-1)
+	fmt.Println(arr1)
 }
